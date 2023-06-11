e3115c4a1a64026c406ec18724349c1db382288a
asemshaath <ashaath@montgomerycollge.edu>
2023-06-10 21:55:37 -0400

    chat gpt done

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import openai
+from dotenv import load_dotenv
+load_dotenv()
+
+openai.organization = "org-kptX8Ha4UeDDlVl9LhWgnpoH"
+openai.api_key = os.getenv('OPENAI_API_KEY')
+openai.Model.list()
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Hi can you help me with some stuff",
+  temperature=0.9,
+  max_tokens=150,
+  top_p=1,
+  frequency_penalty=0.0,
+  presence_penalty=0.6,
+)
+
+print(response.choices[0].text)
